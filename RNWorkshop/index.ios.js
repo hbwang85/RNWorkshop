07ad388c1675f16085953b526c0f97240dc675c7
Haibin Wang <haibin.wang@rea-group.com>
2016-07-26 18:02:34 +1000

    Add Navigator

--- RNWorkshop/index.ios.js
+++ RNWorkshop/index.ios.js
@@ -9,24 +9,25 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  NavigatorIOS,
 } from 'react-native';
+
+class Helloworld extends Component {
+  render() {
+    return(<Text style={styles.text}>Hello World</Text>)
+  }
+}
 
 class Project extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'List',
+          component: Helloworld,
+        }}/>
     );
   }
 }
@@ -34,20 +35,13 @@ class Project extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  text: {
+    color: 'black',
+    backgroundColor: 'red',
+    fontSize: 40,
+    margin: 80,
+  }
 });
 
 AppRegistry.registerComponent('Project', () => Project);
